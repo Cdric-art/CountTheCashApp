9a412e69a35e0b2f4031bd0a9e0a6c96fceab6ca
Cdric_art <cdric.art@gmail.com>
2023-01-13 22:37:41 +0100

    Initial Commit

---
+++ CountTheCashApp/CountTheCashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountTheCashAppApp.swift
+//  CountTheCashApp
+//
+//  Created by Cédric Evrard on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CountTheCashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
